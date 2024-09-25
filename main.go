b1f63a39aeb63274551f1b749dca937c5c0c8d5e
Nicolai Ort <info@nicolai-ort.com>
2024-09-25 07:33:06 +0200

    refactor: Extract main logic into functions

--- main.go
+++ main.go
@@ -10,53 +10,65 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+var rootMessage string
+var port string
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(rootMessage))
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
+
+func handleWhatsMyIP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(strings.Split(r.RemoteAddr, ":")[0]))
+}
+
+func handleHeaders(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Header {
+		w.Write([]byte(k + ": " + strings.Join(v, ", ") + "\n"))
+	}
+}
+func handleStatus(w http.ResponseWriter, r *http.Request) {
+	status, err := strconv.Atoi(r.URL.Path[len("/status/"):])
+	if err != nil {
+		http.Error(w, "Invalid status code", http.StatusBadRequest)
+		return
+	}
+	http.Error(w, http.StatusText(status), status)
+}
+
+func initialize() {
 	err := godotenv.Load()
 	if err != nil && !os.IsNotExist(err) {
 		log.Println("Error loading .env file:", err)
 		os.Exit(1)
 	}
 
-	port := os.Getenv("PORT")
+	port = os.Getenv("PORT")
 	if port == "" {
 		log.Println("PORT not found in env - defaulting to 8080")
 		port = "8080"
 	}
 
-	rootMessage := os.Getenv("ROOT_MESSAGE")
+	rootMessage = os.Getenv("ROOT_MESSAGE")
 	if rootMessage == "" {
 		log.Println("ROOT_MESSAGE not found in env - defaulting to 'Hello, World!'")
 		rootMessage = "Hello, World!"
 	}
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(rootMessage))
-	})
+func main() {
+	initialize()
 
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok"))
-	})
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/healthz", handleHealthz)
+	http.HandleFunc("/whatsmyip", handleWhatsMyIP)
+	http.HandleFunc("/headers", handleHeaders)
+	http.HandleFunc("/status/", handleStatus)
 
-	http.HandleFunc("/whatsmyip", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(strings.Split(r.RemoteAddr, ":")[0]))
-	})
-
-	http.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			w.Write([]byte(k + ": " + strings.Join(v, ", ") + "\n"))
-		}
-	})
-
-	http.HandleFunc("/status/", func(w http.ResponseWriter, r *http.Request) {
-		status, err := strconv.Atoi(r.URL.Path[len("/status/"):])
-		if err != nil {
-			http.Error(w, "Invalid status code", http.StatusBadRequest)
-			return
-		}
-		http.Error(w, http.StatusText(status), status)
-	})
-
-	err = http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
